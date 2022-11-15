4e50bb9f356ea7485137f0a3e86e9d5e053a74e4
Emirhan Hasanoglu <emirhanhasanoglu@protonmail.com>
2022-11-15 16:42:06 +0300

    AppImage Icon Objects for Player Page

--- lib/presentation/app_ui/app_image/app_image.dart
+++ lib/presentation/app_ui/app_image/app_image.dart
@@ -16,4 +16,16 @@ class AppImage {
   final String iconAdd = '${_icons}icon_add.png';
   final String iconSearchOutlined = '${_icons}icon_search_outlined.png';
   final String iconGridLayout = '${_icons}icon_grid_layout.png';
+  final String iconHeartOutlined = '${_icons}icon_heart_outlined.png';
+  final String iconHeartFilled = '${_icons}icon_heart_filled.png';
+  final String iconPlayerPause = '${_icons}icon_player_pause.png';
+  final String iconPlayerSkip = '${_icons}icon_player_skip.png';
+  final String iconPlayerBack = '${_icons}icon_player_back.png';
+  final String iconPlayerShuffle = '${_icons}icon_player_shuffle.png';
+  final String iconPlayerShuffleActive = '${_icons}icon_player_shuffle_active.png';
+  final String iconRepeat = '${_icons}icon_repeat.png';
+  final String iconRepeatActive = '${_icons}icon_repeat_active.png';
+  final String iconDevices = '${_icons}icon_devices.png';
+  final String iconDown = '${_icons}icon_down.png';
+  final String iconListPlaying = '${_icons}icon_list_playing.png';
 }
