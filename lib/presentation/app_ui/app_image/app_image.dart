f363b3ce2edba50778647760b1ea0fcaac5632a7
Emirhan Hasanoglu <emirhanhasanoglu@protonmail.com>
2022-11-24 15:27:44 +0300

    Changing over to new icons

--- lib/presentation/app_ui/app_image/app_image.dart
+++ lib/presentation/app_ui/app_image/app_image.dart
@@ -18,15 +18,15 @@ class AppImage {
   final String iconGridLayout = '${_icons}icon_grid_layout.png';
   final String iconHeartOutlined = '${_icons}icon_heart_outlined.png';
   final String iconHeartFilled = '${_icons}icon_heart_filled.png';
-  final String iconPlayerPause = '${_icons}icon_player_pause.png';
-  final String iconPlayerSkip = '${_icons}icon_player_skip.png';
-  final String iconPlayerBack = '${_icons}icon_player_back.png';
-  final String iconPlayerShuffle = '${_icons}icon_player_shuffle.png';
-  final String iconPlayerShuffleActive = '${_icons}icon_player_shuffle_active.png';
+  final String iconPlayerPause = '${_icons}icon_pause.png';               
+  final String iconPlayerSkip = '${_icons}icon_next.png';
+  final String iconPlayerBack = '${_icons}icon_previous.png';
+  final String iconPlayerShuffle = '${_icons}icon_shuffle.png';
+  final String iconPlayerShuffleActive = '${_icons}icon_shuffle_active.png';
   final String iconRepeat = '${_icons}icon_repeat.png';
   final String iconRepeatActive = '${_icons}icon_repeat_active.png';
   final String iconDevices = '${_icons}icon_devices.png';
   final String iconDown = '${_icons}icon_down.png';
-  final String iconListPlaying = '${_icons}icon_list_playing.png';
+  final String iconListPlaying = '${_icons}icon_queue.png';
   final String iconMenuDots = '${_icons}icon_menu_dots.png';
 }
