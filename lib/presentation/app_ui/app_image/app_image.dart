5459778eb3eb5e0b31ac9e076030142747ff3c3b
Emirhan Hasanoglu <emirhanhasanoglu@protonmail.com>
2022-11-25 15:10:24 +0300

    Rename icon variables

--- lib/presentation/app_ui/app_image/app_image.dart
+++ lib/presentation/app_ui/app_image/app_image.dart
@@ -18,11 +18,11 @@ class AppImage {
   final String iconGridLayout = '${_icons}icon_grid_layout.png';
   final String iconHeartOutlined = '${_icons}icon_heart_outlined.png';
   final String iconHeartFilled = '${_icons}icon_heart_filled.png';
-  final String iconPlayerPause = '${_icons}icon_pause.png';               
-  final String iconPlayerSkip = '${_icons}icon_next.png';
-  final String iconPlayerBack = '${_icons}icon_previous.png';
-  final String iconPlayerShuffle = '${_icons}icon_shuffle.png';
-  final String iconPlayerShuffleActive = '${_icons}icon_shuffle_active.png';
+  final String iconPause = '${_icons}icon_pause.png';               
+  final String iconSkip = '${_icons}icon_next.png';
+  final String iconBack = '${_icons}icon_previous.png';
+  final String iconShuffle = '${_icons}icon_shuffle.png';
+  final String iconShuffleActive = '${_icons}icon_shuffle_active.png';
   final String iconRepeat = '${_icons}icon_repeat.png';
   final String iconRepeatActive = '${_icons}icon_repeat_active.png';
   final String iconDevices = '${_icons}icon_devices.png';
